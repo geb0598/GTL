045f19b959d7e0f42b3d1080456208022b940c46
geb0598 <geb0598@gmail.com>
2025-09-22 23:48:30 +0900

    feat: FArchive 추가

---
+++ Engine/Source/Core/Public/Archive.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <type_traits>
+
+#include "Global/CoreTypes.h"
+#include "Global/Vector.h"
+
+struct FArchive
+{
+	virtual ~FArchive() = default;
+
+	/** Returns true if this archive is for loading data. */
+	virtual bool IsLoading() const = 0;
+	virtual void Serialize(void* V, size_t Length) = 0;
+
+	template<typename T>
+	FArchive& operator<<(T& Value)
+	{
+		if constexpr (std::is_trivially_copyable_v<T>)
+		{
+			Serialize(&Value, sizeof(T));
+		}
+		else
+		{
+			T::Serialize(*this, Value);
+		}
+		return *this;
+	}
+
+	template<typename T>
+	FArchive& operator<<(TArray<T>& Value)
+	{
+		size_t Length = Value.size();
+		if (!IsLoading())
+		{
+			*this << Length;
+		}
+		else
+		{
+			*this << Length;
+			Value.resize(Length);
+		}
+
+		for (T& Element : Value)
+		{
+			*this << Element;
+		}
+		return *this;
+	}
+
+	FArchive& operator<<(FString& Value)
+	{
+		size_t Length = Value.size();
+		if (!IsLoading())
+		{
+			*this << Length;
+			Serialize(Value.data(), Length * sizeof(FString::value_type));
+		}
+		else
+		{
+			*this << Length;
+			Value.resize(Length);
+			Serialize(Value.data(), Length * sizeof(FString::value_type));
+		}
+		return *this;
+	}
+};
